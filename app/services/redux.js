2dbd770fbc30f671577bfb945f257a2f3e2385cd
toran billups <toranb@gmail.com>
2017-06-24 16:00:43 -0500

    an alternative look at configuration with the ReduxService

--- app/services/redux.js
+++ app/services/redux.js
@@ -2,18 +2,23 @@ import redux from 'redux';
 import ReduxService from 'ember-redux/services/redux';
 import createSagaMiddleWare from 'redux-saga';
 import reducers from '../reducers/index';
+import enhancers from '../enhancers/index';
 import root from '../sagas/index';
 
 const { createStore, applyMiddleware, compose } = redux;
 
 const sagaMiddleware = createSagaMiddleWare();
 
+const makeStoreInstance = ({reducers, enhancers}) => {
+  const middleware = applyMiddleware(sagaMiddleware);
+  const createStoreWithMiddleware = compose(middleware, enhancers)(createStore);
+  const store = createStoreWithMiddleware(reducers);
+  sagaMiddleware.run(root);
+  return store;
+};
+
 export default ReduxService.extend({
-  makeStoreInstance() {
-    const middleware = applyMiddleware(sagaMiddleware);
-    const createStoreWithMiddleware = compose(middleware)(createStore);
-    const store = createStoreWithMiddleware(reducers);
-    sagaMiddleware.run(root);
-    return store;
-  }
+  reducers,
+  enhancers,
+  makeStoreInstance
 });
